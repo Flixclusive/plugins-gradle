be3cecffffab9e2b50e4fd8a6bded82fe033a22d
rhenwinch <rhenwinchester@gmail.com>
2025-01-05 02:22:04 +0800

    build(deps): add missing compose compiler plugin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,17 @@ dependencies {
     compileOnly("org.ow2.asm:asm:9.4")
     compileOnly("org.ow2.asm:asm-tree:9.4")
     compileOnly("com.github.vidstige:jadb:master-SNAPSHOT")
+    
+    val agp = "8.7.0"
+    val coreStubs = "1.2.3"
+    val kotlin = "2.0.20"
+    val shadow = "9.0.0-beta4"
 
-    implementation("com.android.tools.build:gradle:8.7.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-    implementation("com.gradleup.shadow:shadow-gradle-plugin:9.0.0-beta4")
-    implementation("com.github.flixclusiveorg.core-stubs:model-provider:1.2.3")
+    compileOnly("com.android.tools.build:gradle:$agp")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin")
+    implementation("com.github.flixclusiveorg.core-stubs:model-provider:$coreStubs")
+    implementation("com.gradleup.shadow:shadow-gradle-plugin:$shadow")
+    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlin")
 }
 
 gradlePlugin {
