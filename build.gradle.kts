527d01efeb86d2ebb129c74c835c295f69ac1d42
rhenwinch <rhenwinchester@gmail.com>
2025-02-09 18:06:32 +0800

    build(deps): convert some `compileOnly` deps to `implementation` mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,20 @@ kotlin {
 }
 
 dependencies {
-    compileOnly(gradleApi())
-    compileOnly("com.google.guava:guava:30.1.1-jre")
-    compileOnly("com.android.tools:sdk-common:31.0.0")
-    compileOnly("org.ow2.asm:asm:9.4")
-    compileOnly("org.ow2.asm:asm-tree:9.4")
-    compileOnly("com.github.vidstige:jadb:master-SNAPSHOT")
-    
     val agp = "8.7.3"
     val coreStubs = "1.2.4"
     val kotlin = "2.0.21"
     val shadow = "9.0.0-beta4"
 
+    compileOnly(gradleApi())
+    compileOnly("com.google.guava:guava:30.1.1-jre")
+    compileOnly("com.android.tools:sdk-common:31.0.0")
     compileOnly("com.android.tools.build:gradle:$agp")
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin")
+
+    implementation("org.ow2.asm:asm:9.4")
+    implementation("org.ow2.asm:asm-tree:9.4")
+    implementation("com.github.vidstige:jadb:master-SNAPSHOT")
     implementation("com.github.flixclusiveorg.core-stubs:model-provider:$coreStubs")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:$shadow")
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlin")
